fb4d04221297214888a6072fb3b60926364980be
Paul Cho <47371078+pauldev1128@users.noreply.github.com>
2021-07-05 14:26:22 -0700

    created render portion on the app pg

--- yelp-project-react/src/App.js
+++ yelp-project-react/src/App.js
@@ -2,9 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 export default class App extends Component {
-  construction(props) {
-    super(props);
-    
+   constructor(props) {
+     super(props); 
+     this.state = {
+       page : 'homepage',
+       signup : false,
+       login: false,
+       restaurants : []
+     }
+   }
+
+   
+
+   render() {
+    switch (this.state.page){
+      case 'update':
+      return (
+        <div><p>This is the update</p></div>  
+      )
+      case 'login':
+      return (
+        <div><p>This is the login</p></div>  
+      )
+      case 'signup':
+      return (
+        <div><p>This is the signup</p></div>  
+      )
+      default:
+      return (
+        <div> This is the homepage</div>
+      )   
+   }
   }
-  
 }
