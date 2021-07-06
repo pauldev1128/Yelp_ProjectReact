addf90beaa116c2f5cc5f8c0979f5a7c3ea3b8a9
cmart1997 <christmartinez1997@yahoo.com>
2021-07-06 09:21:21 -0700

    updated login

--- yelp-project-react/src/components/Login.jsx
+++ yelp-project-react/src/components/Login.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
-const Login = () => {
+const Login = ({setToken}) => {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
     return(
-        <div>
+        <div className= "login">
+            <h1>LOGIN HERE</h1>
             <form>
+                <label>Username: </label>
+                <input type = "text" placeholder="Enter Your Username" name = "username" onChange={e => setUserName(e.target.value)}/>
+                <br/>
 
+                <label>Password: </label>
+                <input type = "passsword" placeholder="Enter Your Password" name = "password" onChange={e => setPassword(e.target.value)}/>
+                <br/>
+                <button type="submit">LOGIN</button> 
             </form>
         </div>
     )
